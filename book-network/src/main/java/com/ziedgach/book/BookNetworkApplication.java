a32fad5f552b241eabbe4a541aa32ddc51c129ac
gachita zied <gachita.zied@esprit.tn>
2024-08-08 17:04:49 +0100

    first commit

---
+++ book-network/src/main/java/com/ziedgach/book/BookNetworkApplication.java
@@ -0,0 +1,34 @@
+package com.ziedgach.book;
+
+import com.ziedgach.book.role.Role;
+import com.ziedgach.book.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class BookNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookNetworkApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository)
+	{
+		return args -> {
+			if (roleRepository.findByName("USER").isEmpty())
+			{
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+
+	}
+
+}
